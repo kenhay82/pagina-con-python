40795a614acd7b075dbf20a2523d26e6e1370990
carlos <Carlosk20@gmail.com>
2024-02-21 12:57:22 -0500

    Preparando para subir a heroku

---
+++ index.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template
+
+app  = Flask(__name__)
+app.static_folder = 'static'
+
+@app.route('/')
+def index():    
+    return render_template('index.html')
+
+@app.route('/productos')
+def productos():
+    return render_template('productos.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
